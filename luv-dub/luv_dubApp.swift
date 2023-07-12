89db7b052fdfb30115347cededc831596a0128de
Jihyuk Song <real.dassy@gmail.com>
2023-07-12 15:17:33 +0900

    프로젝트 초기설정

--- luv-dub/luv_dubApp.swift
+++ luv-dub/luv_dubApp.swift
@@ -5,10 +5,22 @@
 //  Created by 김예림 on 2023/07/12.
 //
 
+import Firebase
+import KakaoSDKAuth
+import KakaoSDKCommon
 import SwiftUI
+
+
 
 @main
 struct luv_dubApp: App {
+    
+    init() {
+        let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
+        KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
