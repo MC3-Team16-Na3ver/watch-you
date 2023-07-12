df5521755a98007e645352b63cb67aa736e40ec9
Moon <59304977+mun9769@users.noreply.github.com>
2023-07-12 23:25:05 +0900

    feat: 로컬에서 notification 기능 구현
    NotificationContent를 생성함.
    UNTimeIntervalNotificationTrigger를 통해 5초후에 노티가 옴

--- luv-dub Watch App/ContentView.swift
+++ luv-dub Watch App/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack
+        {
+            Button("Request permission")
+            {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]) { (success, error) in
+                    if success{
+                        print("All set")
+                    } else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            Button("Schedule Notification")
+            {
+                let content = UNMutableNotificationContent()
+                content.title = "Drink some milk!"
+                content.subtitle = "you have 5 sec"
+                content.sound = .default
+                content.categoryIdentifier = "myCategory"
+                
+                let category = UNNotificationCategory(identifier: "myCategory", actions: [], intentIdentifiers: [], options: [])
+                UNUserNotificationCenter.current().setNotificationCategories([category])
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                let request = UNNotificationRequest(identifier: "milk", content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request) { (error) in
+                    if let error = error{
+                        print(error.localizedDescription)
+                    }else{
+                        print("scheduled successfully")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
