b954c1c76f3825f943b125b9ae16aab0b8e78bb9
Kim, Yerim <yerim343@naver.com>
2023-07-12 14:30:04 +0900

    Initial Commit

---
+++ luv-dub Watch App/luv_dubApp.swift
@@ -0,0 +1,17 @@
+//
+//  luv_dubApp.swift
+//  luv-dub Watch App
+//
+//  Created by 김예림 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct luv_dub_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ luv-dub/luv_dubApp.swift
@@ -0,0 +1,17 @@
+//
+//  luv_dubApp.swift
+//  luv-dub
+//
+//  Created by 김예림 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct luv_dubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
