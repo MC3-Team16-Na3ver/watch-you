db0a59a7d352b06b12c3632f383b05013f622cd1
Moon <59304977+mun9769@users.noreply.github.com>
2023-07-17 17:27:26 +0900

    Revert "feat: 알림과 진동이 함께 오는 기능"

--- luv-dub/luv_dubApp.swift
+++ luv-dub/luv_dubApp.swift
@@ -6,106 +6,24 @@
 //
 
 import Firebase
-import FirebaseMessaging
 import KakaoSDKAuth
 import KakaoSDKCommon
 import SwiftUI
-import UserNotifications
+
+
 
 @main
 struct luv_dubApp: App {
-
-    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     init() {
         let kakaoAppKey = Bundle.main.infoDictionary?["KAKAO_NATIVE_APP_KEY"] ?? ""
         KakaoSDK.initSDK(appKey: kakaoAppKey as! String)
-        
+        FirebaseApp.configure()
     }
     
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-    }
-}
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    let gcmMessageIDKey = "gcm.message_id"
-    let aps = "aps"
-    let data1Key = "DATA1"
-    let data2Key = "DATA2"
-    
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        FirebaseApp.configure()
-        
-        Messaging.messaging().delegate = self
-        
-        UNUserNotificationCenter.current().delegate = self
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        
-        UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: {_, _ in })
-        
-        application.registerForRemoteNotifications()
-        return true
-    }
-    
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        print("userInfo:", userInfo)
-        completionHandler(UIBackgroundFetchResult.newData)
-    }
-}
-
-extension AppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        
-        let userInfo = notification.request.content.userInfo
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID: \(messageID)")
-        }
-        
-        if let data1 = userInfo[data1Key] {
-            print("data1: \(data1)")
-        }
-        
-        if let data2 = userInfo[data1Key] {
-            print("data1: \(data2)")
-        }
-        
-        if let apsData = userInfo[aps] {
-            print("apsData: \(apsData)")
-        }
-        
-        completionHandler([[.banner, .badge, .sound]])
-    }
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Messaging.messaging().apnsToken = deviceToken
-    }
-    
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    }
-    
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
-        let userInfo = response.notification.request.content.userInfo
-        
-        if let messageID = userInfo[gcmMessageIDKey] {
-            print("Message ID from usernotificationCenter didReceive: \(messageID)")
-        }
-        
-        completionHandler()
-    }
-}
-
-extension AppDelegate: MessagingDelegate {
-    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
-        print("messaging")
-        
-        let deviceToken: [String:String] = ["token": fcmToken ?? ""]
-        print("Device token: ", deviceToken)
-
     }
 }
