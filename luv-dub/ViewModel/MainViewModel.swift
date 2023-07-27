18b2d468cde499f5f469f1c59539031577339880
Eric030406 <real.dassy@gmail.com>
2023-07-28 08:26:21 +0900

    fix: getRefreshToken 함수에 completion handler 추가
    - 시간 순차적이어야 하는(Refresh Token을 성공적으로 가져와야 다음 함수들이 정상적으로 실행됨) 함수에 completion handler를 추가

--- luv-dub/ViewModel/MainViewModel.swift
+++ luv-dub/ViewModel/MainViewModel.swift
@@ -5,8 +5,11 @@
 //  Created by Song Jihyuk on 2023/07/17.
 //
 
+import CoreData
 import Firebase
 import FirebaseAuth
+import FirebaseDatabase
+import FirebaseDatabaseSwift
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 import SwiftUI
@@ -15,10 +18,36 @@ class MainViewModel: ObservableObject {
     @Published var myData = User(name: "", nickname: "", dDay: "", userID: "", email: "", deviceToken: "", connectedID: "")
     @Published var loverData = User(name: "", nickname: "", dDay: "", userID: "", email: "", deviceToken: "", connectedID: "")
     @Published var path: [ViewType] = []
+    @Published var users: [UserInfo] = []
     
-    func fetchDatas() {
+    func getRefreshToken(completion: @escaping (String) -> Void) {
+        Database.database().reference().child("notification/refresh_token").getData { error, snapShot in
+            if let error = error {
+                print(error.localizedDescription)
+                completion("")
+                return
+            }
+            
+            if let refreshToken = snapShot?.value as? String {
+                completion(refreshToken)
+            }
+          }
+    }
+    
+    func addUserIdToRealtimeDatabase(deviceToken: String, loverUid: String, loversDeviceToken: String) {
+        guard let currentUser = Auth.auth().currentUser else { return }
+        
+        Database.database().reference().child("User").child(currentUser.uid).setValue([
+            "deviceToken": deviceToken,
+            "loverUid": loverUid,
+            "loversDeviceToken": loversDeviceToken,
+        ])
+    }
+    
+    func fetchDatas(completion: @escaping () -> Void) {
         let db = Firestore.firestore().collection("User")
         guard let currentUserUid = Auth.auth().currentUser?.uid else { return }
+        
         db.document(currentUserUid).getDocument(as: User.self) { result in
             switch result {
             case .success(let user):
@@ -32,6 +61,7 @@ class MainViewModel: ObservableObject {
                     switch data {
                     case .success(let lover):
                         self.loverData = lover
+                        completion()
                     case .failure(let error):
                         print(error)
                     }
