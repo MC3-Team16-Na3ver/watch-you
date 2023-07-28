6bfb985e3e3a55b65c4e887922a57c0a1c0d5484
Moon <59304977+mun9769@users.noreply.github.com>
2023-07-28 23:06:45 +0900

    feat: Notification권한요청 로직 구현
    AppDelegate는 앱의 라이프사이클을 관리한다.
    앱이 실행될 때 AppDelegate가 알림권한을 확인하기 위해 AppDelegate에게 ViewModel 역할을 부여함.

--- luv-dub/luv_dubApp.swift
+++ luv-dub/luv_dubApp.swift
@@ -30,6 +30,7 @@ struct luv_dubApp: App {
             ContentView()
                 // .environment(\.managedObjectContext, dataController.container.viewContext)
                 .environmentObject(LoginViewModel())
+                .environmentObject(AppDelegate())
                 .onOpenURL { url in
                     if (AuthApi.isKakaoTalkLoginUrl(url)) {
                         _ = AuthController.handleOpenUrl(url: url)
@@ -46,14 +47,16 @@ class AppDelegate: NSObject, UIApplicationDelegate {
     let data2Key = "DATA2"
 
 
+    @Published var isAlertOn: Bool = false
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         
         Messaging.messaging().delegate = self
         
-        UNUserNotificationCenter.current().delegate = self
-        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        checkNotificationPermission()
         
         application.registerForRemoteNotifications()
         return true
