ab14ef09bb40e882f57d715979595ff5cd22c553
Moon <59304977+mun9769@users.noreply.github.com>
2023-09-16 00:31:06 +0900

    feat: User 도메인 수정
    `User`은 하나만 필요하므로 싱글톤으로 만들었다.
    회원가입하는 과정에서 개인정보를 받기 때문에 전부 옵셔널 타입으로 선언했다.

--- luv-dub/Model/User.swift
+++ luv-dub/Model/User.swift
@@ -8,27 +8,18 @@
 import FirebaseFirestoreSwift
 import SwiftUI
 
-struct User: Identifiable, Codable {
-    @DocumentID var id: String?
-    let name: String
-    var nickname: String
-    let dDay: String
-    var userID: String
-    let email: String
-    var deviceToken: String
-    var connectedID: String
-    var invitationCode: String
+class User: Encodable, Decodable {
+    static let shared = User()
     
-    enum CodingKeys: String, CodingKey {
-        case id
-        case name
-        case nickname
-        case dDay
-        case userID
-        case email
-        case deviceToken
-        case connectedID
-        case invitationCode
-    }
+    @DocumentID var id: String?
+    var name: String?
+    var nickname: String?
+    var dDay: String?
+    var userID: String?
+    var email: String?
+    var deviceToken: String?
+    var connectedID: String?
+    var invitationCode: String?
+    
     
 }
